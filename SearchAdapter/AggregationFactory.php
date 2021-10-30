a8fb46ec36854bac3770ea8e123c72a664dca53d
Tadeu Rodrigues <karlostadeu85@gmail.com>
2021-10-30 02:08:51 -0300

    created aggregation factory

--- SearchAdapter/AggregationFactory.php
+++ SearchAdapter/AggregationFactory.php
@@ -5,6 +5,8 @@ namespace TadeuRodrigues\Solr\SearchAdapter;
 
 use Magento\Framework\ObjectManager\ObjectManager;
 use Magento\Framework\Search\Response\Aggregation;
+use Magento\Framework\Search\Response\Aggregation\Value;
+use Magento\Framework\Search\Response\Bucket;
 
 class AggregationFactory
 {
@@ -28,13 +30,44 @@ class AggregationFactory
     public function create(array $rawAggregation): Aggregation
     {
         // TODO: implementar aggregation module-elasticsearch/SearchAdapter/AggregationFactory.php:40
+        /** @var Bucket[] $buckets */
         $buckets = [
             ['name' => '', 'values' => '']
         ];
+        foreach ($rawAggregation as $rawBucketName => $rawBucket) {
+            $buckets[$rawBucketName] = $this->objectManager->create(
+                Bucket::class,
+                [
+                    'name' => $rawBucketName,
+                    'values' => $this->prepareValues($rawBucket)
+                ]
+            );
+        }
 
         return $this->objectManager->create(
             Aggregation::class,
             ['buckets' => $buckets]
         );
+    }
+
+    /**
+     * @param array $values
+     * @return Value[]
+     */
+    private function prepareValues(array $values): array
+    {
+        /** @var Value[] $valuesObjects */
+        $valuesObjects = [];
+        foreach ($values as $name => $value) {
+            $valuesObjects[] = $this->objectManager->create(
+                Value::class,
+                [
+                    'value' => $name,
+                    'metrics' => $value
+                ]
+            );
+        }
+
+        return $valuesObjects;
     }
 }
